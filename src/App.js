b6b8c1ebc15a874b966517c93e153106ac542dac
Madop1 <madhan.murthy@10decoders.in>
2023-10-05 12:32:07 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+import StatementInfo from './viewStatement/StatementInfo';
+
+function App() {
+  return (
+    <div className="container">
+      <StatementInfo/>
+    </div>
+  );
+}
+
+export default App;
